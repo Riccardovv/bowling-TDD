194e0e928bf51610591bf926e10609d6f9de9c67
Puerto Manuel <manuel.puerto@solera.com>
2022-10-17 15:09:52 +0200

    test definitions

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -11,4 +11,16 @@ public class App
 
         System.out.println( "Hello World!" );
     }
+
+    public  static void rollTest(){
+        //roll set the number of pins knocked down.
+
+    }
+
+    public  static void scoreTest(){
+        //return the score
+    }
+
+
+
 }
